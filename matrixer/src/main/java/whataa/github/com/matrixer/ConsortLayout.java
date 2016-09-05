6e9330fd414ba5864ac027601aec228907ff90de
a598235157 <y837979117@gmail.com>
2016-09-05 23:53:57 +0800

    mVelocityTracker

--- matrixer/src/main/java/whataa/github/com/matrixer/ConsortLayout.java
+++ matrixer/src/main/java/whataa/github/com/matrixer/ConsortLayout.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
@@ -15,10 +16,11 @@ public class ConsortLayout extends ViewGroup {
     public static final String TAG = ConsortLayout.class.getSimpleName();
 
     private Scroller mScroller;
+    private VelocityTracker mVelocityTracker;
     private View headerView;
     private View contentView;
     private int mScrollRange;
-    private int mCurrTop;
+    private float mCurrTop;
     private float mCurrOffset;
     private int mTouchSlop;
 
@@ -55,7 +57,6 @@ public class ConsortLayout extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        Log.i(TAG,"onMeasure");
         measureChildren(widthMeasureSpec, heightMeasureSpec);
 
         int maxWidth = MeasureSpec.getSize(widthMeasureSpec);
@@ -67,7 +68,6 @@ public class ConsortLayout extends ViewGroup {
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        Log.i(TAG,"onSizeChanged");
         if (w != oldw || h != oldh) {
             mScrollRange = headerView.getMeasuredHeight();
         }
@@ -75,8 +75,7 @@ public class ConsortLayout extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        Log.i(TAG,"onLayout");
-        headerView.layout(l, mCurrTop, r, mCurrTop + headerView.getMeasuredHeight());
+        headerView.layout(l, (int) mCurrTop, r, (int) (mCurrTop + headerView.getMeasuredHeight()));
         contentView.layout(l, t, r, b);
         // init the position of contentView's content.
         contentView.setPadding(0, headerView.getBottom(), 0, 0);
@@ -84,24 +83,28 @@ public class ConsortLayout extends ViewGroup {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        Log.i(TAG,"onInterceptTouchEvent");
+        Log.i(TAG, "onInterceptTouchEvent");
         switch (ev.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
-                Log.i(TAG,"onInterceptTouchEvent | ACTION_DOWN");
+                Log.i(TAG, "onInterceptTouchEvent | ACTION_DOWN");
                 lastX = ev.getX();
                 lastY = ev.getY();
-                if (isScrolling()) {
-                    Log.i(TAG,"onInterceptTouchEvent | ACTION_DOWN: "+isScrolling());
-                    return true;
+                if (mVelocityTracker == null) {
+                    mVelocityTracker = VelocityTracker.obtain();
                 }
+                mVelocityTracker.addMovement(ev);
+//                if (isScrolling()) {
+//                    Log.i(TAG,"onInterceptTouchEvent | ACTION_DOWN: "+isScrolling());
+//                    return true;
+//                }
                 break;
             case MotionEvent.ACTION_MOVE:
-                Log.i(TAG,"onInterceptTouchEvent | ACTION_MOVE "
-                        + lastY +" "
-                        +ev.getY()+" "
-                        +isHeaderHide()+" "
-                        +isContentViewTop()+" "
-                        +mTouchSlop);
+                Log.i(TAG, "onInterceptTouchEvent | ACTION_MOVE "
+                        + lastY + " "
+                        + ev.getY() + " "
+                        + isHeaderHide() + " "
+                        + isContentViewTop() + " "
+                        + mTouchSlop);
                 float diffX = ev.getX() - lastX;
                 float diffY = ev.getY() - lastY;
                 if (Math.abs(diffX) > Math.abs(diffY) || Math.abs(diffY) < mTouchSlop) {
@@ -125,22 +128,27 @@ public class ConsortLayout extends ViewGroup {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
+//        if (isScrolling()) {
+//            return true;
+//        }
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        mVelocityTracker.addMovement(ev);
         switch (ev.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
-                Log.i(TAG,"onTouchEvent | ACTION_DOWN");
-                if (isScrolling()) {
-                    mScroller.abortAnimation();
-                }
+                Log.i(TAG, "onTouchEvent | ACTION_DOWN");
                 return true;
             case MotionEvent.ACTION_MOVE:
-                Log.i(TAG,"onTouchEvent | ACTION_MOVE");
-                computeCurrVal((int) (ev.getY() - lastY));
-                lastY = ev.getY();
+                Log.i(TAG, "onTouchEvent | ACTION_MOVE");
+                computeCurrVal(mCurrTop + ev.getY() - lastY);
                 headerView.requestLayout();
+                lastY = ev.getY();
                 break;
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP:
-                determineTargetLoc(0,0,0,0);
+                mVelocityTracker.computeCurrentVelocity(1000);
+                determineTarget(ev);
                 break;
         }
         return true;
@@ -153,38 +161,34 @@ public class ConsortLayout extends ViewGroup {
     private boolean isContentViewTop() {
         return contentView.getScrollY() == 0;
     }
+
     private boolean isScrolling() {
         return !mScroller.isFinished();
     }
-    private void computeCurrVal(int dy) {
-        mCurrTop = Math.max(Math.min(mCurrTop +dy, getPaddingTop()), -mScrollRange);
-        // TODO 待解决：导致mCurrOffset无法等于1
-//        if (Math.abs(mCurrTop - mScrollRange) < 3) {
-//            mCurrTop = -mScrollRange;
-//        }
-        mCurrOffset = Math.abs((float) mCurrTop / (float) mScrollRange);
-        Log.i(TAG,"computeCurrVal "+dy+" -"+mScrollRange+" "+ mCurrTop +" "+ mCurrOffset);
+
+    private void computeCurrVal(float currY) {
+        mCurrTop = Math.max(Math.min(currY, getPaddingTop()), -mScrollRange);
+        mCurrOffset = Math.abs(mCurrTop / mScrollRange);
+        Log.i(TAG, "computeCurrVal " + currY + " -" + mScrollRange + " " + mCurrTop + " " + mCurrOffset);
     }
-    private void determineTargetLoc(int currX, int currY, float xVel, float yVel) {
-        // depend on speed & location.
-        lastY = headerView.getBottom();
-        if (mCurrOffset >= 0.3f) {
-            mScroller.startScroll(headerView.getLeft(), headerView.getBottom(), 0, -headerView.getBottom());
+
+    private void determineTarget(MotionEvent ev) {
+        float dy = 0;
+        if (mVelocityTracker.getYVelocity() < 0) {
+            dy = -mScrollRange - mCurrTop;
         } else {
-            mScroller.startScroll(headerView.getLeft(), headerView.getBottom(), 0, mScrollRange-headerView.getBottom());
+            dy = getPaddingTop() - mCurrTop;
         }
+        mScroller.startScroll(headerView.getLeft(), (int) mCurrTop, 0, (int) dy);
         invalidate();
     }
 
     @Override
     public void computeScroll() {
         if (mScroller.computeScrollOffset()) {
-            computeCurrVal((int) (mScroller.getCurrY() - lastY));
+            computeCurrVal(mScroller.getCurrY());
             headerView.requestLayout();
-            lastY = mScroller.getCurrY();
-            postInvalidate();
-        } else {
-
+            invalidate();
         }
     }
 }
